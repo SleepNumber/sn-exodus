bfc243ff8f5820e383e69511ed1a9d441c9dc14a
Ian McKay <tehmuffzor@gmail.com>
2023-10-02 14:05:54 -0500

    SNCOM-1202 build

--- dist/esm/util/core/hub.js
+++ dist/esm/util/core/hub.js
@@ -1910,6 +1910,9 @@ function sub(topic, listener) {
   // Add the listener to topic's listener queue
   const index = TOPIC_LISTENERS[topic].push(listener) - 1;
 
+  // We're subbed to and ready to be pubbed to
+  mod.topicDfds[topic]?.resolve(listener);
+
   // Provide handle back for removal of a topic listener
   return {
     remove: () => {
@@ -1962,6 +1965,7 @@ function getLogEnabled() {
 }
 const mod = {
   pub,
+  sub,
   toggleLogging: () => {
     const shouldLog = getLogEnabled();
     _cookiejar__WEBPACK_IMPORTED_MODULE_3__/* ["default"] */ .Z.set(_cookiejar__WEBPACK_IMPORTED_MODULE_3__/* ["default"] */ .Z.Entry.hub_log_enabled, !shouldLog);
@@ -2009,11 +2013,6 @@ mod.onTopicListener = topic => {
   /** @type {Deferred} */
   const dfd = mod.topicDfds[topic];
   return dfd.promise();
-};
-mod.sub = (topic, listener) => {
-  const result = sub(topic, listener);
-  mod.topicDfds[topic]?.resolve(listener);
-  return result;
 };
 (0,_object__WEBPACK_IMPORTED_MODULE_7__/* .namespace */ .uD)('sn.hub', mod);
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (mod);
